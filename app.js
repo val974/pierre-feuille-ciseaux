e132b5d0b0d02609232cd3c0e9a1e1eccc243aaa
val974 <valerie.dij@gmail.com>
2017-06-25 18:43:48 +0400

    comparaison des différents choix de l'utilisateur et de l'ordinateur

--- app.js
+++ app.js
@@ -28,4 +28,33 @@ var comparer =function(choix1,choix2) {
 	           alert( "la feuille enveloppe la pierre, vous avez perdu !");
 	 		}
 	   	}
+
+	   	 if (choix1 === choix2) {
+	        alert ("Vous êtes à égalité !");
+	    }
+
+		else if (choix1==="feuille") {
+			if (choix2 === "pierre") {
+				alert( "la feuille enveloppe la pierre, vous avez remporté la partie");
+			}
+		
+			else {
+				alert( "les ciseaux coupe la feuille, vous avez perdu !");
+			}
+	  		}
+
+	  	 if (choix1 === choix2) {
+		        alert ("Vous êtes à égalité !");
+		    }
+
+			else if (choix1==="ciseaux") {
+				if (choix2==="pierre") {
+					alert( "la pierre écrase les ciseaux, vous avez perdu" );
+				}
+				else {
+					alert( "les ciseaux coupe la feuille,vous avez remporté la partie" );
+				}
+			}
+
 }
+comparer (choixutilisateur,choixOrdi);
