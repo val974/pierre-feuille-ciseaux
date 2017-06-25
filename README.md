ba643925db0e5c1c94223d55a1ff197fb4ae2ae0
val974 <valerie.dij@gmail.com>
2017-06-25 18:35:05 +0400

    choix de l'utilisateur et de l'ordinateur

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pierre-feuille-ciseaux
+
+Pour ce jeu c'est l'homme vs la machine.
+
+Le jeu suit différentes étapes :
+
+Le jeu demande au joueur d'entrer son nom
+Le jeu propose au joueur de choisir entre : pierre, feuille, ciseaux
+l'IA choisit aléatoirement entre les 3 possibilités
+Le jeu compare les choix et affiche qui a gagné entre l'IA et le joueur.
+Le jeu propose au joueur de rejouer
---
+++ app.js
@@ -0,0 +1,14 @@
+var nomutilisateur=prompt ("entrer votre nom");
+var choixutilisateur = prompt ("Choisissez entre pierre,feuille et ciseaux");
+var choixOrdi = Math.random ();
+
+	if (Math.random <= 0.30) {
+	    choixOrdi = alert ("l'ordinateur choisit la pierre");
+	}
+	else if (Math.random >0.31 <0.60) {
+	    ChoixOrdi = alert ("l'ordinateur choisit feuille");
+	}
+	else {
+	     ChoixOrdi =alert ("l'ordinateur choisit ciseaux");
+	}
+
