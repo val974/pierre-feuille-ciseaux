ad53a491a11747ca54327e169f59e5ae1cd5cb71
val974 <valerie.dij@gmail.com>
2017-06-25 18:40:00 +0400

    ajout de la fonction comparer

--- app.js
+++ app.js
@@ -6,9 +6,26 @@ var choixOrdi = Math.random ();
 	    choixOrdi = alert ("l'ordinateur choisit la pierre");
 	}
 	else if (Math.random >0.31 <0.60) {
-	    ChoixOrdi = alert ("l'ordinateur choisit feuille");
+	    ChoixOrdi = alert ("l'ordinateur choisit la feuille");
 	}
 	else {
-	     ChoixOrdi =alert ("l'ordinateur choisit ciseaux");
+	     ChoixOrdi =alert ("l'ordinateur choisit les ciseaux");
 	}
 
+var comparer =function(choix1,choix2) {
+
+	    if (choix1 === choix2) {
+	        alert ("Vous êtes à égalité !");
+	    }
+
+	   	else if (choix1 === "pierre") {
+	       
+	        if (choix2 ==="ciseaux"){
+	           alert( "la pierre écrase les ciseaux !, vous avez remporté la partie !");
+	       	}
+
+	       else {
+	           alert( "la feuille enveloppe la pierre, vous avez perdu !");
+	 		}
+	   	}
+}
